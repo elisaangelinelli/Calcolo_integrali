310ad0c5b5ca8c40026725b0333f030a8b56fd1e
Elisa Angelinelli <angelinelli.elisa@gmail.com>
2024-03-06 23:08:41 +0100

    New version of math.h and math.c

--- mathf.c
+++ mathf.c
@@ -1,32 +1,30 @@
-/*! @file mathf.c
-    @brief Definitions of the fuctions
-    @author Elisa Angelinelli
+/** \file mathf.c
+	\brief defintions of the functions declared in mathf.h
+	\author Paolo Gastaldo
 
-    Details.
+	Details.
 */
 
 #include "mathf.h"
 
+
 /*! \brief computes the output value of a polynomial
-	\param coeff coefficients of the polynomial (pointer to) 
-	\param size length of the coeff array 
+	\param polyf the polynomial
 	\param in the input value 
 	\return the output value
-	
-	the polynomial is structured as follows: coeff[0] + coeff[1]*x + ....
 */
-float Polynomial(float* coeff, int size, float in) {
+float Polynomial(poly_s polyf, float in) {
 	
-	int i; 				/* counter for loops */
-	float out = 0.;		/* used as accumulator to compute the value of the function given an input */
-	float x = in;		/* to represent the different elements of the polynomial (x, x^2, x^3,...) */	
+	int i;
+	float out = 0.;
+	float x = in;	
 	
-	out = coeff[0];
-	for (i=1; i<size; i++) {
-		out += coeff[i]*x;
-		x *= in;			/* x^i */
+	out = polyf.coeffs[0];
+	for (i=1; i<polyf.degree; i++) {
+		out += polyf.coeffs[i]*x;
+		x *= in;
 	}
-    
+
 	return out;
 
 }
@@ -39,11 +37,11 @@ float Polynomial(float* coeff, int size, float in) {
 	\param integ1 integral computed using as reference the first point of each interval 
 	\param integ2 integral computed using as reference the second point of each interval	
 */
-void Rectangular(float* values, int size, float stepsize, float* integ1, float* integ2) /*passaggio per reference poiché altrimenti dovevamo fare una struct*/{
+void Rectangular(float* values, int size, float stepsize, float* integ1, float* integ2) {
 	
-	int i; 	/* counter for loops */
+	int i;
 
-	(*integ1) = 0.; 	
+	(*integ1) = 0.;
 	(*integ2) = 0.;	
 	
 	(*integ1) += stepsize*values[0];
@@ -61,14 +59,14 @@ void Rectangular(float* values, int size, float stepsize, float* integ1, float* 
 	\param values points that set the equally spaced intervals (pointer to) 
 	\param size length of the values array 
 	\param stepsize the interval between two points 
-	\return the computed integral  
+	\return the integral computed  
 */
 float Trapezoidal(float* values, int size, float stepsize) {
 	
-	int i; 		/* counter for loops */
+	int i;
 
-	float integ = 0.; 		/* used as accumulator to compute the integral */
-	float h = stepsize/2.;	/* to avoid dividing by 2 at each round of the loop */
+	float integ = 0.;
+	float h = stepsize/2.;
 	
 	for (i=0; i<(size-1); i++) 
 		integ += h*(values[i+1]+values[i]);
--- mathf.h
+++ mathf.h
@@ -1,21 +1,23 @@
 /*! @file mathf.h
-    @brief Library of functions for computing integrals
-    @author Elisa Angelinelli
+	@brief Library of functions for computing integrals 
+	@author Paolo Gastaldo
 
-    Details.
-*/
+	Details.
+*/ 
 
-#ifndef MATHF_H
+
+#ifndef MATHF_H 
 #define MATHF_H
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> 
 #include <string.h>
+#include "structs.h"
 
-float Polynomial(float* coeff, int size, float in);
+float Polynomial(poly_s polyf, float in); 
+void Rectangular(float* values, int size, float stepsize, float* integ1, float* integ2); 
+float Trapezoidal(float* values, int size, float stepsize); 
 
-void Rectangular(float* values, int size, float stepsize, float* integ1, float* integ2);
-
-float Trapezoidal(float* values, int size, float stepsize);
+ 
 
 #endif

932c68249cba31f7bf94f05f7e7d1ea822b0b625
Elisa Angelinelli <angelinelli.elisa@gmail.com>
2024-03-06 23:13:43 +0100

    Updated the old versions of mathf's library

---
+++ mathf_vecchio.h
@@ -0,0 +1,21 @@
+/*! @file mathf.h
+    @brief Library of functions for computing integrals
+    @author Elisa Angelinelli
+
+    Details.
+*/
+
+#ifndef MATHF_H
+#define MATHF_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+float Polynomial(float* coeff, int size, float in);
+
+void Rectangular(float* values, int size, float stepsize, float* integ1, float* integ2);
+
+float Trapezoidal(float* values, int size, float stepsize);
+
+#endif
